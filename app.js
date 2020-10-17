9ef57e4b7c98e650339fa0bfdc619f5c5535a109
Abhay Kumar <abhay.kumar@geminisolutions.in>
2020-10-17 16:56:25 +0530

    added hostname and version

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-require('dotenv').config()
-const express = require('express')
-const http = require('http')
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const os = require("os");
 
-const app = express()
-const connectToDatabase = require('./db')
-const Note = require('./Note')
+const app = express();
+const connectToDatabase = require("./db");
+const Note = require("./Note");
 
-app.get('/', async (req, res) => {
-    await connectToDatabase()
-    const notes = await Note.find()
-    res.send({ v: 1, notes })
-})
+app.get("/", async (req, res) => {
+  await connectToDatabase();
+  const notes = await Note.find();
+  res.send({ v: 2, os: os.hostname, notes });
+});
 
-http.createServer(app).listen(process.env.PORT || 8000)
+http.createServer(app).listen(process.env.PORT || 8000);
